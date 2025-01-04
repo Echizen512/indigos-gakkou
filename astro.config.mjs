d68247515728760ef6dc2ecb0d2d0306dfa91e85
Echizen512 <jmrm19722@gmail.com>
2025-01-03 21:58:37 -0400

    prueba3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: "https://i.postimg.cc/QC74WPrc/",
+  site: "https://i.postimg.cc/QC74WPrc",
   integrations: [
     tailwind(),
     mdx(),
